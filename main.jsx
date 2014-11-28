369be777bf0bee27925ec3e13c7b3dd77ae99236
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2014-11-27 23:36:49 -0500

    Rebuild a estado actual.

--- main.jsx
+++ main.jsx
@@ -1,126 +1,121 @@
 'use strict';
 
 var ImageGrid = React.createClass({
+    propTypes: {
+        width: React.PropTypes.number,
+        height: React.PropTypes.number,
+        data: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                src: React.PropTypes.string,
+                text: React.PropTypes.string
+            })),
+
+        imageRatio: React.PropTypes.number,
+        imageMaxWidth: React.PropTypes.number.isRequired,
+        imageMinWidth: React.PropTypes.number.isRequired
+    },
     getDefaultProps: function(){
         return{
-            data: [],
-            imgRatio: 1,
-            imgMaxWidth: 300,
-            imgMinWidth: 200,
-            style: {
-                position: 'absolute',
-                left: 'auto',
-                top: 0,
-            },
+            imageRatio: 1
         };
-    },
-    getInitialState: function(){
-        return{
-            width: 0,
-        };    
     },
     render: function(){
-        // Primer renderizado
-        if (! this.isMounted()){
-            var style = React.addons.update(this.props.style,
-                                            {position: {$set: 'relative'}});
-            return(
-                <div id='imageGrid' style={style}
-                     ref='divNode'>
-                </div>
-            );
+        var gridStyle = {
+            position: 'absolute'
         };
-
         var data = this.props.data;
-        var gridWidth = this.state.width;
-        var imgMaxWidth = this.props.imgMaxWidth;
-        var imgMinWidth = this.props.imgMinWidth;
+        var gridWidth = this.props.width;
+        var imgMaxWidth = this.props.imageMaxWidth;
+        var imgMinWidth = this.props.imageMinWidth;
         var numColumns = Math.floor(gridWidth/ imgMinWidth);
+        console.log(numColumns);
         var imgWidth = gridWidth/numColumns;
         if (imgWidth>imgMaxWidth) imgWidth = imgMaxWidth;
+        var imgHeight = imgWidth*this.props.imageRatio;
+
         var i = 0;
         var j = 0;
-        var imgProps = { width: imgWidth,
-                         height: imgWidth * this.props.imgRatio};
-        var imageList = this.props.data.map(function(child){
-            var imgCoords = {x: i, y: j};
-            i++;
-            if (i == numColumns){
-                i = 0;
-                j++;
+        var imageList = data.map(function(item){
+            if (j == numColumns){ j=0; i++;};
+            var position = {
+                x: j*imgWidth,
+                y: i*imgHeight
             };
-            return <ImageCell {...child}
-                              {...imgProps}
-                              imgCoords={imgCoords}
-                              />;
+            j++;
+            var imageProps = {
+                width: imgWidth,
+                height: imgHeight,
+                position: position
+            };
+            return <ImageCell {...imageProps} {...item}/>;
         });
-        return(
-            <div id='imageGrid'
-                 ref='divNode'
-                 style={this.props.style}>
+
+        return (
+            <div style={gridStyle}>
                 {imageList}
             </div>
         );
-    },
-    getDivWidth: function(){
-        var divNode = this.refs.divNode.getDOMNode();
-        var rect = divNode.parentNode.getBoundingClientRect();
-        this.setState({width:rect.width});
-    },
-    componentDidMount: function(){
-        this.getDivWidth();
-        window.addEventListener('resize', this.getDivWidth);
-    },
+    }
+
 });
 
 var ImageCell = React.createClass({
-    getDefaultProps:function(){
+    propTypes: {
+        src: React.PropTypes.string,
+        href: React.PropTypes.string,
+        width: React.PropTypes.number.isRequired,
+        height: React.PropTypes.number.isRequired,
+        title: React.PropTypes.string,
+        subtitle: React.PropTypes.string,
+        position: React.PropTypes.shape({
+            x: React.PropTypes.number,
+            y: React.PropTypes.number
+        })
+    },
+    getDefaultProps: function(){
         return{
-            src: 'http://placehold.it/200x200',
-            width: 200,
-            height: 200,
-            href: '#',
-            title: 'Placeholder text',
-            subtitle: 'something',
-            imgCoords: {x: 0, y: 0},
+            src:'http://placehold.it/200x200',
+            text: ''
         };
     },
-    onClickHandler: function(event){
-        window.location.href = this.props.href;
-    },
-
     render: function(){
-        var imgProps = { src: this.props.src,
-                         width: this.props.width,
-                         height: this.props.height,
-                         style:{
-                             
-                         },
+        var imgProps = {
+            src: this.props.src,
+            width: this.props.width,
+            height: this.props.height
         };
-        var imgCoords = this.props.imgCoords;
-        var wrapProps = {style: { position: 'absolute',
-                                  left: imgCoords.x * this.props.width,
-                                  top: imgCoords.y * this.props.height,
-                                  cursor: 'pointer',
-        },};
-        var captionProps = {
-            
+        var overLayerStyle = {
+            backgroundColor: 'black',
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            opacity: 0.4,
+            filter: 'alpha(opacity=40)'
+        };
+        var wrapStyle = {
+            position: "absolute",
+            left: this.props.position.x,
+            top: this.props.position.y
+        };
+        var captionStyle = {
+            position: 'absolute',
+            bottom: '5%',
+            left: '5%'
+        };
+        var titleStyle = {
+            color: 'white'
         };
         return(
-            
-            <div className='imageCellWrap'
-                 {...wrapProps}
-                 onClick={this.onClickHandler}
-                 >
-                <img className='imageCell'
-                     {...imgProps} />
-                <div className='caption'
-                     {...captionProps}>
-                    <p>{this.props.title}</p>
-                    <span>{this.props.subtitle}</span>
+            <div style={wrapStyle}>
+                <img {...imgProps}/>
+                <div style={overLayerStyle}></div>
+                <div style={captionStyle}>
+                    <p style={titleStyle}>{this.props.title}</p>
+                    <p>{this.props.subtitle}</p>
                 </div>
             </div>
         );
-    },
+    }
 });
-
